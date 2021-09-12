f767ac22f6cda81ed87355fd7986f896e09c3ce4
marcelosoliveira <msbobsk8@gmail.com>
2021-09-12 07:21:54 -0300

    Foi renomeada a pasta components para components-learn

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './components/hello/hello.component';
-import { WelcomeComponent } from './components/welcome/welcome.component';
-import { AddNameComponent } from './components/add-name/add-name.component';
+import { HelloComponent } from './components-learn/hello/hello.component';
+import { WelcomeComponent } from './components-learn/welcome/welcome.component';
+import { AddNameComponent } from './components-learn/add-name/add-name.component';
 import { FormsModule } from '@angular/forms';
-import { PersonCardComponent } from './components/person-card/person-card.component';
-import { PersonFormComponent } from './components/person-form/person-form.component';
+import { PersonCardComponent } from './components-learn/person-card/person-card.component';
+import { PersonFormComponent } from './components-learn/person-form/person-form.component';
 
 @NgModule({
   declarations: [
