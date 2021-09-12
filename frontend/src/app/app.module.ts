25ce7aa0b283a8b59a2a96d36739e88ed40da2cb
marcelosoliveira <msbobsk8@gmail.com>
2021-09-12 14:34:07 -0300

    Foi implementado o template da aplicação app-algamoney.component.html

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { TabViewModule } from 'primeng/tabview';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './components-learn/hello/hello.component';
 import { WelcomeComponent } from './components-learn/welcome/welcome.component';
 import { AddNameComponent } from './components-learn/add-name/add-name.component';
-import { FormsModule } from '@angular/forms';
 import { PersonCardComponent } from './components-learn/person-card/person-card.component';
 import { PersonFormComponent } from './components-learn/person-form/person-form.component';
 
@@ -24,7 +27,10 @@ import { PersonFormComponent } from './components-learn/person-form/person-form.
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    TabViewModule
+    TabViewModule,
+    InputTextModule,
+    ButtonModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
