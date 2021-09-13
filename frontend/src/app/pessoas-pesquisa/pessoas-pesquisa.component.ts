aa37bce3fc6fda2cf2c4095d36fadbd9d6a80a44
marcelosoliveira <msbobsk8@gmail.com>
2021-09-13 08:08:00 -0300

    Foi implementado a função ativar e desativar no componente pessoas

--- frontend/src/app/pessoas-pesquisa/pessoas-pesquisa.component.ts
+++ frontend/src/app/pessoas-pesquisa/pessoas-pesquisa.component.ts
@@ -8,14 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class PessoasPesquisaComponent implements OnInit {
 
   public pessoas = [
-    { nome: "Manoel Pinheiro", cidade: "Urbelândia", estado: "MG", status: "Ativo" },
-    { nome: "Sebastião da SIlva", cidade: "São Paulo", estado: "SP", status: "Inativo" },
-    { nome: "Carla Souza", cidade: "Florianópolis", estado: "SC", status: "Ativo" },
-    { nome: "Luís Ferreira", cidade: "Curitiba", estado: "PR", status: "Ativo" },
-    { nome: "Vilmar Andrade", cidade: "Rio de Janeiro", estado: "RJ", status: "Inativo" },
-    { nome: "Joaquim de Oliveira", cidade: "Carangola", estado: "MG", status: "Inativo" },
-    { nome: "Ana Márcia dos Santos", cidade: "Lindóia", estado: "SP", status: "Ativo" },
-    { nome: "Marcelo dos Santos", cidade: "Lindóia", estado: "SP", status: "Ativo" }
+    { nome: "Manoel Pinheiro", cidade: "Urbelândia", estado: "MG", status: true },
+    { nome: "Sebastião da SIlva", cidade: "São Paulo", estado: "SP", status: false },
+    { nome: "Carla Souza", cidade: "Florianópolis", estado: "SC", status: true },
+    { nome: "Luís Ferreira", cidade: "Curitiba", estado: "PR", status: true },
+    { nome: "Vilmar Andrade", cidade: "Rio de Janeiro", estado: "RJ", status: false },
+    { nome: "Joaquim de Oliveira", cidade: "Carangola", estado: "MG", status: false },
+    { nome: "Ana Márcia dos Santos", cidade: "Lindóia", estado: "SP", status: true },
+    { nome: "Marcelo dos Santos", cidade: "Lindóia", estado: "SP", status: true }
   ]
 
   constructor() { }
