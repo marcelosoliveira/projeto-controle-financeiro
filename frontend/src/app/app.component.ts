12e33aabe93bcbb05d13240ec5ca673821075901
marcelosoliveira <msbobsk8@gmail.com>
2021-09-28 17:45:02 -0300

    Foi feita a validação de campos no componente cidades-form

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app-algamoney.component.html',
+  templateUrl: './app-cidades.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
