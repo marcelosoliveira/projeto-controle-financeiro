b182b7dbf570957ac1dee168b688fa9cf3372c97
marcelosoliveira <msbobsk8@gmail.com>
2021-10-08 15:28:09 -0300

    Iniciandop a implementacao de pessoas-service

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { LearnsModule } from './learns/learns.module';
-import { PessoaModule } from './pessoa/pessoa.module';
+import { PessoaModule } from './pessoa/pessoas.module';
 import { LancamentoModule } from './lancamento/lancamento.module';
 import { CoreModule } from './core/core.module';
 
